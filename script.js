bce7e6a0c3c20030a3df536ecaf76873ec0ace2c
Anne <anne.dupin91@gmail.com>
2021-04-27 09:48:25 +0200

    First

---
+++ script.js
@@ -0,0 +1,33 @@
+$("#courier-font").click(function(){
+    $('#font-change').css("font-family", "Courier Prime");
+})
+
+$("#courgette-font").click(function(){
+    $('#font-change').css("font-family","Courgette");
+  });
+  
+  $("#ubuntu-font").click(function(){
+    $('#font-change').css("font-family","Ubuntu");
+  });
+  
+  $("#chewy-font").click(function(){
+    $('#font-change').css("font-family","Chewy");
+  });
+  
+  $("#kaushan-script-font").click(function(){
+    $('#font-change').css("font-family","Kaushan Script");
+  });
+  
+  $("#lobster-two-font").click(function(){
+    $('#font-change').css("font-family","Lobster Two");
+  });
+  
+  $("#economia-font").click(function(){
+    $('#font-change').css("font-family","Economica");
+  });
+  
+  $("#satisfy-font").click(function(){
+    $('#font-change').css("font-family","Satisfy");
+  });
+  
+  
